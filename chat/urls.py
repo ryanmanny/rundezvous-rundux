ffb1fd63656737c263f7c54ce01c53373b283989
ryanmanny <ryan.manny@wsu.edu>
2019-04-16 11:09:11 -0700

    Rename send_message view

--- chat/urls.py
+++ chat/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('chatroom', views.chatroom, name='chatroom'),
     path('chatroom/new_messages/<int:last_message_id>', views.new_messages, name='new_messages'),
 
-    path('message', views.message, name='post_message'),
+    path('message', views.message, name='send_message'),
     path('message/<int:message_id>', views.message, name='get_message'),
 ]
