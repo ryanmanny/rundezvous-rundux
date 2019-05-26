daa311d2598a4e7d4b25b04ebbc110cb8fbbfe63
ryanmanny <ryan.manny@wsu.edu>
2019-05-26 14:35:36 -0700

    Fix up const a little

--- rundezvous/const.py
+++ rundezvous/const.py
@@ -7,10 +7,10 @@ USER_ACTIVE_TIME = datetime.timedelta(days=7)
 
 # The time after which all Rundezvouses will be assumed expired
 MAX_RUNDEZVOUS_EXPIRATION = datetime.timedelta(hours=1)
-# Chat time limit
-CHAT_TIME_LIMIT = datetime.timedelta(minutes=2)
-
 # The maximum distance between two users where a meetup can still be created
-MEETUP_DISTANCE_THRESHOLD = Distance(m=100)
+MEETUP_DISTANCE_THRESHOLD = Distance(m=800)  # About half a mile, quarter each
 
+# Self-explanatory
 MAX_CHAT_MESSAGE_LENGTH = 140
+# How long users can chat before the Rundezvous Decision must be made
+CHAT_TIME_LIMIT = datetime.timedelta(minutes=2)
